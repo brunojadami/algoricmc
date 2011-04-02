74535288c9d1a5ec430fcd362844c8ede7aeee3b
101b147ch@gmail.com <101b147ch@gmail.com@ae3ed244-9829-11de-b8e1-0bcae535652c>
2011-04-02 22:09:54 +0000

    Melhorias no Kadane.

--- ninho/src/kadane.cpp
+++ ninho/src/kadane.cpp
@@ -3,16 +3,16 @@
 #include<algorithm>
 using namespace std;
 
-inline void snippet(int *curMax, int elem, int *result){
-	*curMax += elem;
-	*curMax = max(0, *curMax);
-	*result = max(*curMax, *result);
+inline void snippet(int &curMax, int elem, int &result){
+	curMax += elem;
+	curMax = max(0, curMax);
+	result = max(curMax, *result);
 }
 
 int kadane(vector<int> &vec){
 	int result = 0, curMax = 0;
 	for(int i = 0; i < vec.size(); i++)
-		snippet(&curMax, vec[i], &result);
+		snippet(curMax, vec[i], result);
 	return result;
 }
 
@@ -29,7 +29,7 @@ int kadane2D(int matrix[][MAX_COLS], int nRows, int nCols){
 			subSums[i][j] = matrix[i][j];
 			if(likely(i > 0)) subSums[i][j] += subSums[i-1][j];
 
-			snippet(&curMax, subSums[i][j], &maxTmp);
+			snippet(curMax, subSums[i][j], maxTmp);
 		}
 		result = max(result, maxTmp);
 	}
@@ -38,7 +38,7 @@ int kadane2D(int matrix[][MAX_COLS], int nRows, int nCols){
 		for(int k = i+1; k < nRows; k++){
 			maxTmp = curMax = 0;
 			for(int j = 0; j < nCols; j++)
-				snippet(&curMax, subSums[k][j] - subSums[i][j], &maxTmp);
+				snippet(curMax, subSums[k][j] - subSums[i][j], maxTmp);
 			result = max(result, maxTmp);
 		}
 	return result;
