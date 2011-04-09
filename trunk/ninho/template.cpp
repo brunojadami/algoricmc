cd4feef620bec5c7d63af89dba14517243524902
brunojadami <brunojadami@ae3ed244-9829-11de-b8e1-0bcae535652c>
2011-04-09 16:43:36 +0000

    Coloquei mais dois arquivos que vou arrumar e por mais coisa ainda, o que tiver certo vai dentro do src, o que tiver incompleto fica fora do src..

---
+++ trunk/ninho/template.cpp
@@ -0,0 +1,8 @@
+
+
+
+bool isvowel (char ch)
+{
+    if ('A' == ch || 'a' == ch ||'E' == ch || 'e' == ch ||'I' == ch || 'i' == ch ||'O' == ch || 'o' == ch ||'U' == ch || 'u' == ch)  return true;
+    else return false;        
+}
