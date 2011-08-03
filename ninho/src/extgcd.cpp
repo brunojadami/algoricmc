039281d7e488f07292f3e3020cbad8b462a3cb5d
brunojadami <brunojadami@ae3ed244-9829-11de-b8e1-0bcae535652c>
2011-08-03 00:38:00 +0000

    Adicionando euclidiano extendido e uma explicacao de como funciona e para usar com equacoes lineares diophantinas.

---
+++ ninho/src/extgcd.cpp
@@ -0,0 +1,37 @@
+/*
+	*** Euclidiano extendido, tambem para resolver equacoes diophantinas lineares ***
+	
+	a*x + b*y = c
+	
+	- se c = gcd(a, b) usar o euclidiano extendido
+	- se gcd(a, b) nao divide c nao ha resposta
+	- se gcd(a, b) divide c ha resposta: 
+		eh so usar o euclidiano extendido e mutiplicar a resposta (x, y) por c/gcd(a, b)
+	- se a ou b forem negativos, fazer com o modulo deles e multiplicar a
+		respectiva resposta por -1 (x se a for negativo e y se b for negativo)
+	- vai achar a solucao que minimiza |x|+|y| primeiro e x <= y segundo, para solucao geral:
+	
+		x = x0 + b*t/g                   t eh inteiro!!
+		y = y0 - a*t/g
+*/
+
+#define LL long long
+
+// normal gcd
+LL gcd(LL a, LL b)
+{
+	if (!b) return a;
+	return gcd(b, a%b);
+}
+
+// extended euclidean
+void gcdEx(LL a, LL b, LL& x, LL& y, LL& g)
+{
+	if (!b) { x = 1; y = 0; g = a; return; }
+    	gcdEx(b, a%b, x, y, g);
+    	LL x1 = y;
+    	LL y1 = x-(a/b)*y;
+    	x = x1;
+    	y = y1;
+}
+
