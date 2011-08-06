d474848371ca5426a2b643cc0d2c723a735eb852
brunojadami@gmail.com <brunojadami@gmail.com@ae3ed244-9829-11de-b8e1-0bcae535652c>
2011-08-06 16:42:03 +0000

    Adicionando uns comentarios a mais do euclidiano extendido e a funcao de mod.

--- ninho/src/extgcd.cpp
+++ ninho/src/extgcd.cpp
@@ -7,7 +7,7 @@
 	- se gcd(a, b) nao divide c nao ha resposta
 	- se gcd(a, b) divide c ha resposta: 
 		eh so usar o euclidiano extendido e mutiplicar a resposta (x, y) por c/gcd(a, b)
-	- se a ou b forem negativos, fazer com o modulo deles e multiplicar a
+	- se a ou b forem negativos, fazer com o abs deles e multiplicar a
 		respectiva resposta por -1 (x se a for negativo e y se b for negativo)
 	- vai achar a solucao que minimiza |x|+|y| primeiro e x <= y segundo, para solucao geral:
 	
@@ -17,14 +17,20 @@
 
 #define LL long long
 
-// normal gcd
+// modulo pra qualquer numero (positivo ou negativo).. as funcoes abaixo so utilizam numeros positivos
+int mod(int a, int m)
+{
+  return (a%m+m)%m;
+}
+
+// normal gcd, a e b positivos
 LL gcd(LL a, LL b)
 {
 	if (!b) return a;
 	return gcd(b, a%b);
 }
 
-// extended euclidean
+// extended euclidean, a e b positivos
 void gcdEx(LL a, LL b, LL& x, LL& y, LL& g)
 {
 	if (!b) { x = 1; y = 0; g = a; return; }
